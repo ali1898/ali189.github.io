215124c352523fe8df6fd34584f682dddf819bb1
AliAhmadiNasab <aliahmadinasab81@gmail.com>
2025-06-07 13:26:49 +0330

    slide show is complate

--- js/script.js
+++ js/script.js
@@ -1,45 +1,10 @@
-// -----------------------------Slide Show------------------------------
-let slideEl = document.getElementsByClassName("slide");
-let nextEl = document.querySelector(".next");
-let prevEl = document.querySelector(".prev");
-//Number of slide
-let n = 0;
-
-function displayNone() {
-  for (let i = 0; i < slideEl.length; i++) {
-    slideEl[i].style.display = "none";
-  }
-}
-
-function noActive() {
-  for (let i = 0; i < slideEl.length; i++) {
-    slideEl[i].classList.remove("active");
-  }
-}
-
-const nextNav = () => {
-  n++;
-  if (n > slideEl.length - 1) {
-    n = 0;
-  }
-  displayNone();
-  noActive();
-  slideEl[n].style.display = "block";
-  slideEl[n].classList.add("active");
-};
-const prevNav = () => {
-  n--;
-  if (n < 0) {
-    n = slideEl.length - 1;
-  }
-  displayNone();
-  noActive();
-  slideEl[n].style.display = "block";
-  slideEl[n].classList.add("active");
-};
-nextEl.addEventListener("click", nextNav);
-prevEl.addEventListener("click", prevNav);
-
-setInterval(() => {
-  nextNav();
-}, 4000);
+// Initialize swiper instance
+const swiper = new Swiper(".slider-container", {
+  effect: "fade",
+  speed: 1300,
+  autoplay: { delay: 4000 },
+  navigation: {
+    prevEl: "#slide-prev",
+    nextEl: "#slide-next",
+  },
+});
